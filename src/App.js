27d815fddc39b59a70c1a102f7f66c48c975487f
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2021-02-19 11:35:55 +0000

    ADD: form to add new task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from "react";
+import AddTask from './components/AddTask'
 function App() {
-
+  
+  const [showAddTask, setAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -17,10 +19,40 @@ function App() {
       reminder: true
     }
   ]);
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const NewTask = { id, ...task };
+    setTasks([...tasks, NewTask]);
+   
+  };
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // toggle. Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
-      <Header name='TASK TRACKER' />
-      <Tasks  tasks={tasks} />
+      <Header onAdd={() => setAddTask(!showAddTask)} showAdd={showAddTask} />
+
+ {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                "No Task To Show"
+              )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import Button from './Button';
-const Header = ({name}) => {
+import PropTypes from 'prop-types';
+import Logo from '../assets/checklist.svg';
+import { IoMdAdd, IoMdRemove } from 'react-icons/io';
+const Header = ({ name, showAdd, onAdd }) => {
   return (
     <header class="header">
+      <div class="nav-header">
+        <span></span>
+        <span onClick={onAdd}>{
+          showAdd ? <IoMdRemove style={{color:'red',cursor: 'pointer'}} onClick={onAdd}/> : <IoMdAdd style={{color:'green',cursor: 'pointer'}} onClick={onAdd}/>
+        }
+        </span>
+      </div>
       <div class="nav-user">
+         <div class="user-image">
+          <img src={Logo} alt="React Logo" />
+        </div>
         <div class="user-info">
           <h4 class="user-name">{name}</h4>
         </div>
-        <div class="user-image">
-         
-          <img
-            src="https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
-            alt="Add"
-          />
-        </div>
-         < Button color='green' text='ADD' onClick={() => { console.log('Clicked') }}/>
+       
       </div>
     </header>
   );
 };
 Header.defaultProps = {
-    title: 'TASK TRACKER'
-}
+  name: 'TASK TRACKER',
+};
 Header.propType = {
-    title: PropTypes.string
-}
+  name: PropTypes.string,
+};
 export default Header;
